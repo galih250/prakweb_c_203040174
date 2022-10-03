1b36a50dd9cba409f5985d89cd1683a39067cf78
galih250 <galihk250@gmail.com>
2022-10-03 17:37:16 +0700

    Constructor

---
+++ OOP/Constructor.php
@@ -0,0 +1,28 @@
+<?php
+
+class Produk {
+    public $judul ="judul",
+           $penulis ="penulis",
+           $penerbit ="penerbit",
+           $harga;
+ 
+        public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0) {
+            $this->judul =$judul;
+            $this->penulis =$penulis;
+            $this->penerbit =$penerbit;
+            $this->harga =$harga;
+        }
+
+        public function getLabel() {
+            return "$this->penulis, $this->penerbit";
+        }
+
+        }
+
+    $produk1 = new Produk ("Naruto", "Masashi Kishimoto","Shonwn Jump", 30000);
+    $produk2 = new Produk ("Uncharted","Neil Druckmann", "Sony Computer", 250000);
+    $produk3 = new Produk ("Dragon Ball");
+    
+    echo "Komik : " . $produk1->getLabel();
+    echo "<br>";
+    echo "Game : " . $produk2->getLabel();
