0417cce4e8fab009a9b7be2899cc4555605a588c
galih250 <galihk250@gmail.com>
2022-10-06 23:56:50 +0700

    Menyelesaikan Vidio 3 Routing

--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -1,8 +1,20 @@
 <?php
 
+
 class App{
     public function __construct()
     {
-        echo 'Ok!';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if(isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
